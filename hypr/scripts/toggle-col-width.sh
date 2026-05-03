c74fad40617e699a0d9482ac99bfc5d63d0b11e5
MystiaFin <cytrsx01@gmail.com>
2026-05-03 12:02:01 +0700

    commit

---
+++ hypr/scripts/toggle-col-width.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+STATE_FILE="/tmp/hypr-col-toggle"
+DEFAULT=0.7
+
+if [ -f "$STATE_FILE" ]; then
+    rm "$STATE_FILE"
+    hyprctl dispatch layoutmsg "colresize $DEFAULT"
+else
+    touch "$STATE_FILE"
+    hyprctl dispatch layoutmsg "colresize 1.0"
+fi
